d066b2ea682075b67121ee038e15899084c37e33
Vitória Lucy <vitoria_branco@estudante.sesisenai.org.br>
2024-06-04 10:37:18 -0300

    Pokedex

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Carta from './Componentes/Carta'
+
+function App() {
+
+  return (
+    <>
+      <Carta />
+    </>
+  )
+}
+
+export default App
